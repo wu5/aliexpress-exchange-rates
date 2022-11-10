140c9397fcdc4f796aed1adcac6c8253311febb9
wu5 <rinshirou@gmail.com>
2022-11-10 15:04:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,48 @@
+const axios = require('axios'); 
+const cheerio = require('cheerio'); 
 const express = require("express");
+
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+async function getData() {
+	const scrapedData = [];
+	const tableHeaders = ['date', 'bank', 'ali_ru', 'ali_com', 'gearbest', 'geekbuying', 'banggood'];
+	const url = "https://helpix.ru/currency/";
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+	await axios.get(url).then(({ data }) => { 
+		const $ = cheerio.load(data); // Initialize cheerio 
 
+	$(".b-tabcurr__tr").each((index, element) => {
+		const tds = $(element).find("td");
+		const tableRow = {};
+		$(tds).each((i, element) => {
+		  tableRow[tableHeaders[i]] = $(element).text();
+		});
+		scrapedData.push(tableRow);
+	  });
+  
+	  for (let key in scrapedData[1]) {
+		if (scrapedData[1][key] == '-')
+			scrapedData[1][key] = scrapedData[2][key];
+		if (key != 'date') 
+			scrapedData[1][key] = Number(scrapedData[1][key]);
+			//scrapedData[1][key] = Number(Number(scrapedData[1][key]).toFixed(1));
+	  }
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+	});
+	console.log(scrapedData[1]);
+	return scrapedData[1];
+}	
+
+app.get("/", async (req, res) => {
+	try {
+	  const json = await getData();
+	  return res.status(200).json(json);
+	} catch (err) {
+	  return res.status(500).json({err: err.toString()});
+	}
+  });
+  
+  app.listen(8080, () =>
+  console.log(`The server is active and running on port 80`)
+  );
